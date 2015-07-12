bf879914898555fff6191f4b8667dac1097811a0
fishbdn <florian.rosier@gmail.com>
2015-07-12 21:03:02 +0200

    Init 1st launch - admin fish, mysql db, specific data folder

---
+++ www/fishcloud/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'occ55gcxtxlz',
+  'passwordsalt' => '3OpdMOJfHDvPXGxk20zrarqIl2gu2z',
+  'secret' => '8xG5lG7M3nogt0SlijAAl+Tv/MoP1eWt09Fp77KwtzCpviJn',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.11',
+  ),
+  'datadirectory' => '/home/pi/data/fishcloud',
+  'overwrite.cli.url' => 'http://192.168.1.11/fishcloud',
+  'dbtype' => 'mysql',
+  'version' => '8.1.0.8',
+  'dbname' => 'fishcloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_fish1',
+  'dbpassword' => '9n0qw4tq2xs1g1m42id2k5qnl2ib3f',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
